2ab374d3c82934315ed40ddb1c712abb52a57252
SChinchi <57867641+SChinchi@users.noreply.github.com>
2025-11-27 13:59:28 +0000

    1.2.1 and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+**1.2.1**
+
+* Update for Alloyed Collective.
+
 **1.2.0**
 
 * Added Risk of Options compatibility.
--- SwitchChaosAndGesture/SwitchChaosAndGesture.cs
+++ SwitchChaosAndGesture/SwitchChaosAndGesture.cs
@@ -23,7 +23,7 @@ public class SwitchChaosAndGesture : BaseUnityPlugin
     public const string PluginGUID = PluginAuthor + "." + PluginName;
     public const string PluginAuthor = "Chinchi";
     public const string PluginName = "SwitchChaosAndGesture";
-    public const string PluginVersion = "1.2.0";
+    public const string PluginVersion = "1.2.1";
     internal const string RiskOfOptionsGUID = "com.rune580.riskofoptions";
 
     private static AssetBundle assetBundle;
